18b82e1cd761292981068dac9ab70e3a7fca2a6b
Eli Array Minkoff <eli@planetminkoff.com>
2021-02-07 21:57:28 -0500

    Removed awk requirement

--- pvpnmon.sh
+++ pvpnmon.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # A simple genmon script to display the status of your ProtonVPN connection.
-protonvpn -s | awk '/Status:/ {print($2)}' | grep -q Dis && echo "<img>$HOME/.pvpnmon/disconnected.svg</img>" || echo "<img>$HOME/.pvpnmon/connected.svg</img>"
+protonvpn -s | grep -q Dis && echo "<img>$HOME/.pvpnmon/disconnected.svg</img>" || echo "<img>$HOME/.pvpnmon/connected.svg</img>"
