01ce06eb3cde18906c610c877a673a2eb1c7c416
Eli Array Minkoff <eli@planetminkoff.com>
2021-02-07 21:26:53 -0500

    Update and rename pvpnmon to pvpnmon.sh

--- pvpnmon.sh
+++ pvpnmon.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-# genmon script to display the status of ProtonVPN
+# A simple genmon script to display the status of your ProtonVPN connection.
 protonvpn -s | awk '/Status:/ {print($2)}' | grep -q Dis && echo "<img>$HOME/.pvpnmon/disconnected.svg</img>" || echo "<img>$HOME/.pvpnmon/connected.svg</img>"
-
